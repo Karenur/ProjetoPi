b7003df5971717b83a7cec46e9088397a201ff22
Karenur <mateusbbocchino@gmail.com>
2022-03-22 22:43:36 -0300

    commit 1

--- jogo/Assets/scripts/baralho.cs
+++ jogo/Assets/scripts/baralho.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class baralho : Local
 {
@@ -33,9 +34,8 @@ public class baralho : Local
         
         for (int i = 0; i < posicoesCartas.Count; i++)
         {
-            int rnd = Random.Range(0, bm.cartasDisponiveis.Count);
-            //Instantiate(bm.cartasDisponiveis[rnd],posicoesCartas[i].transform.position,posicoesCartas[i].transform.rotation);
-            posicoesCartas[i].GetComponent<SpriteRenderer>().sprite = bm.cartasDisponiveis[rnd].GetComponent<SpriteRenderer>().sprite;
+            int rnd = Random.Range(0, bm.cartasDisponiveis.Count);            
+            posicoesCartas[i].GetComponent<Image>().sprite = bm.cartasDisponiveis[rnd].GetComponent<SpriteRenderer>().sprite;
             
         }
         

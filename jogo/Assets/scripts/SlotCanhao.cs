eca16c681f121da491d20a386816914f7b2631e8
Karenur <mateusbbocchino@gmail.com>
2022-03-26 17:52:00 -0300

    commit3
    Canhao muda ângulo

--- jogo/Assets/scripts/SlotCanhao.cs
+++ jogo/Assets/scripts/SlotCanhao.cs
@@ -12,34 +12,48 @@ public class SlotCanhao : MonoBehaviour, IDropHandler
     public BancoDeMunicoes bm;
 
     public GameObject canhao;
+    public GameObject saidaCanhao;
 
 
     // Start is called before the first frame update
     void Start()
     {
         _transform = this.GetComponent<RectTransform>();
+        
     }
 
-   public void OnDrop(PointerEventData eventData)
+    public void OnDrop(PointerEventData eventData)
     {
         string nomeCarta = eventData.pointerDrag.GetComponent<Image>().sprite.name;
         //eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = _transform.anchoredPosition;
         Debug.Log($"atirei {nomeCarta}");
         Debug.Log($"usando o {this.name} ");
-        
-        if(nomeCarta == "bolaFerro")
+        if (name == "canhao1")
         {
-            Instantiate(bm.cartasDisponiveis[0], canhao.transform);
+            canhao.transform.rotation = Quaternion.Euler(0,0,0);
+        }
+        if (name == "canhao2")
+        {
+           canhao.transform.rotation = Quaternion.Euler(0,0,15);
+        }
+        if (name == "canhao3")
+        {
+            canhao.transform.rotation = Quaternion.Euler(0,0,45);
+        }
+
+        if (nomeCarta == "bolaFerro")
+        {
+            Instantiate(bm.cartasDisponiveis[0], saidaCanhao.transform);
 
         }
         if (nomeCarta == "broca")
         {
-            Instantiate(bm.cartasDisponiveis[1], canhao.transform);
+            Instantiate(bm.cartasDisponiveis[1], saidaCanhao.transform);
 
         }
         if (nomeCarta == "escudo")
         {
-            Instantiate(bm.cartasDisponiveis[2], canhao.transform);
+            Instantiate(bm.cartasDisponiveis[2], saidaCanhao.transform);
 
         }
 
